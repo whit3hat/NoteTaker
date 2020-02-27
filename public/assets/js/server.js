38cf5c5bc098af4ef0165aee02b0491a0a0978af
Spencer Wood <spencer.wood@me.com>
2020-02-27 16:07:11 -0600

    wrote basic functions for express to start the website on port 3000 and validated it will start.

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,4 +1,30 @@
+//Dependencies
 const fs = require ('fs');
 const express = require ('express');
 const path = require('path');
 
+//Setup Express App
+var app = express();
+var PORT = 3000;
+
+
+//Setup the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+//Routes
+//=============================================
+
+//Basic route that sends the user first to the index page
+app.get('/', function(req, res){
+    res.sendFile(path.join(_dirname, 'index.html'));
+
+
+})
+
+
+//Start the server
+//==========================================
+app.listen(PORT, function(){
+    console.log("App listening on PORT" + PORT)
+});
